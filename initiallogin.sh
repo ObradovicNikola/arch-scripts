b49f056c180aa4452ed4a8113c7390250c2d884f
Obradovic Nikola <obradovicnikola009@gmail.com>
2020-07-28 12:58:51 +0200

    added realtek wifi driver

--- initiallogin.sh
+++ initiallogin.sh
@@ -8,3 +8,14 @@ startx /usr/bin/lxappearance
 # use lxappearance to set cursor theme globaly
 
 # enable sound (unmute)
+
+# for lenovo legion
+`sudo touch /etc/modprobe.d/blacklist.conf`
+`sudo cat "blacklist ideapad_laptop" > /etc/modprobe.d/blacklist.conf`
+
+realtek wifi chip, rtw88 driver from AUR 
+`rtl8822de`
+
+https://aur.archlinux.org/packages/rtw88-dkms-git/
+
+https://github.com/lwfinger/rtw88
